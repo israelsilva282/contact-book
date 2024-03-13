33d877ebba173159ce931e41bfcccd3539312fce
israelsilva282 <israelcostas@hotmail.com>
2024-03-13 09:42:39 -0300

    feat: adicionando rotas de contatos

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ContatoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(ContatoController::class)->group(function () {
+    Route::get('/contatos', 'index');
+    Route::post('/contatos', 'store');
+    Route::get('/contatos/{contato}', 'show');
+    Route::put('/contatos/{contato}', 'update');
+    Route::delete('/contatos/{contato}', 'destroy');
 });
