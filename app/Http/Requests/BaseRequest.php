a383b75a97f1be5897b70f00be08de5c79e822c6
israelsilva282 <israelcostas@hotmail.com>
2024-03-13 15:53:08 -0300

    feat: adicionado request base para funcao de validar erros

---
+++ app/Http/Requests/BaseRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Traits\HttpResponses;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class BaseRequest extends FormRequest
+{
+    use HttpResponses;
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            $this->error('Validation error', 422, $validator->errors())
+
+        );
+    }
+}
